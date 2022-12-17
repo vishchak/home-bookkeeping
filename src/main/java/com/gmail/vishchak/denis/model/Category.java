989a8f5d74e1f129db226e7311e3d40fb5cae6e6
vishchak <denis.vishchak@gmail.com>
2022-12-18 10:30:24 +1100

    update Entities -remove unused annotations

--- src/main/java/com/gmail/vishchak/denis/model/Category.java
+++ src/main/java/com/gmail/vishchak/denis/model/Category.java
@@ -2,7 +2,6 @@ package com.gmail.vishchak.denis.model;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
